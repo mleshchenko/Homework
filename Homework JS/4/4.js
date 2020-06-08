27d73728795b0a6b44ba4f64f3802a926f92a332
Max Leshchenko <mleshchenko@gmail.com>
2020-06-07 19:44:50 -0600

    JSHW6

--- Homework JS/4/4.js
+++ Homework JS/4/4.js
@@ -14,6 +14,16 @@ function createNewUser() {
                 value: prompt('Введите фамилию пользователя')
             })
 
+        },
+        getAge() {
+            const currentDate = new Date();
+            const dateOfBirthArray = this.dateOfBirth.split('.');
+            const dateOfBirthFormatted = new Date(dateOfBirthArray[2],dateOfBirthArray[1]-1, dateOfBirthArray[0]);
+            return Math.trunc(((currentDate - dateOfBirthFormatted)/1000/60/60/24/365));
+        },
+        getPassword() {
+            const dateOfBirthArray = this.dateOfBirth.split('.');
+            return this.firstName[0].toUpperCase()+this.lastName.toLowerCase()+dateOfBirthArray[2];
         }
     }
 
@@ -29,12 +39,20 @@ function createNewUser() {
         configurable: true
     })
 
+    Object.defineProperty(newUser, 'dateOfBirth', {
+        value: prompt('Введите дату вашего рождения в формате - dd.mm.yyyy'),
+        enumerable: true,
+        configurable: true
+    })
+
     return newUser;
 }
 
 let newUser = createNewUser();
 
 console.log(newUser);
+console.log(newUser.getAge());
+console.log(newUser.getPassword());
 
 
 
